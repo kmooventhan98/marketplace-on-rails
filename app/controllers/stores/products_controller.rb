24d77ec1551d7f3e36e613199876001a0fbe1d5d
Enoch Tamulonis <enochtamulonis@gmail.com>
2025-04-05 18:29:44 +0100

    display products on store show page

--- app/controllers/stores/products_controller.rb
+++ app/controllers/stores/products_controller.rb
@@ -27,7 +27,7 @@ module Stores
 
       respond_to do |format|
         if @product.save
-          format.html { redirect_to @product, notice: "Product was successfully created." }
+          format.html { redirect_to [@store, @product], notice: "Product was successfully created." }
           format.json { render :show, status: :created, location: @product }
         else
           format.html { render :new, status: :unprocessable_entity }
